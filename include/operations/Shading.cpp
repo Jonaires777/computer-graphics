a06a622011be3fa8ab81a7a5a0ea53294ed309f5
Jonaires777 <airesjonathan123@gmail.com>
2025-10-07 17:58:09 -0300

    refactor: add compMul operation

--- include/operations/Shading.cpp
+++ include/operations/Shading.cpp
@@ -1,4 +1,5 @@
 #include "operations/Shading.h"
+#include "operations/Operations.h"
 #include <glm/gtc/constants.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <algorithm>
@@ -13,6 +14,8 @@ glm::vec3 shade(
     const Object& hitObject,
     const std::vector<std::unique_ptr<Object>>& sceneObjects
 ) {
+	using namespace Operations;
+    
     glm::vec3 D = glm::normalize(glm::vec3(ray.direction));
     glm::vec3 Pf = glm::vec3(light.position.position);
     glm::vec3 l = glm::normalize(Pf - Pi);
@@ -39,8 +42,8 @@ glm::vec3 shade(
     if (!inShadow) {
         glm::vec3 r = glm::normalize(2.0f * glm::dot(n, l) * n - l);
         glm::vec3 I_F = light.intensity;
-        glm::vec3 I_diff = hitObject.K_diffuse * I_F * glm::max(0.0f, glm::dot(l, n));
-        glm::vec3 I_spec = hitObject.K_specular * I_F * std::pow(glm::max(0.0f, glm::dot(v, r)), hitObject.shininess);
+        glm::vec3 I_diff = compMul(I_F, hitObject.K_diffuse) * glm::max(0.0f, glm::dot(l, n));
+        glm::vec3 I_spec = compMul(I_F, hitObject.K_specular) * std::pow(glm::max(0.0f, glm::dot(v, r)), hitObject.shininess);
         I_diff_spec = I_diff + I_spec;
     }
 
